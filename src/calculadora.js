493d8d7c404d85ed7a2b59f257776ecb60f70aad
Donald-Alvarez <donaldoctavio0210@gmail.com>
2023-06-10 23:19:50 -0600

    Creacion de la estructura de los botones con sus respectivas funcionalidades

--- src/calculadora.js
+++ src/calculadora.js
@@ -24,7 +24,132 @@ const Calculadora = () => {
     setResultado('');
   };
 
-
+  return (
+    
+    <View style={styles.container}>
+      <Text>Mi Calculadora</Text>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>{resultado}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('7')}
+          >
+            <Text style={styles.buttonText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('8')}
+          >
+            <Text style={styles.buttonText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('9')}
+          >
+            <Text style={styles.buttonText}>9</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('4')}
+          >
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('5')}
+          >
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('6')}
+          >
+            <Text style={styles.buttonText}>6</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('1')}
+          >
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('2')}
+          >
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('3')}
+          >
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('0')}
+          >
+            <Text style={styles.buttonText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleButtonPress('.')}
+          >
+            <Text style={styles.buttonText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonO}
+            onPress={() => calcularResultado('=')}
+          >
+            <Text style={styles.buttonText}>=</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.buttonO}
+            onPress={() => handleButtonPress('+')}
+          >
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonO}
+            onPress={() => handleButtonPress('-')}
+          >
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonO}
+            onPress={() => handleButtonPress('*')}
+          >
+            <Text style={styles.buttonText}>*</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.buttonO}
+            onPress={() => handleButtonPress('/')}
+          >
+            <Text style={styles.buttonText}>/</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonB}
+            onPress={() => limpiarResultado('AC')}
+          >
+            <Text style={styles.buttonText}>C</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 };
+
 
 export default Calculadora;
